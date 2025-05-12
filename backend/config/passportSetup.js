987eeda17b6fdae99d632e2a3c1c568bff2bf840
Aradhya <srivastavaaradhya9@gmail.com>
2025-05-12 19:08:19 +0530

    callback URL

--- backend/config/passportSetup.js
+++ backend/config/passportSetup.js
@@ -20,7 +20,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL :
+        process.env.NODE_ENV === "production"
+          ? "https://xeno-crm-backend-10ui.onrender.com/api/auth/google/callback"
+          : "http://localhost:5000/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const { id, displayName, emails, photos } = profile;
--- xeno-crm/src/pages/Login.jsx
+++ xeno-crm/src/pages/Login.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 function Login() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-sm text-center">
-                <h1 className="text-3xl font-bold mb-2 text-gray-800">Login</h1>
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-sm text-center">
+        <h1 className="text-3xl font-bold mb-2 text-gray-800">Login</h1>
 
-                <button
-                    className="w-60 mx-auto mt-5 flex items-center justify-center gap-3 bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-semibold py-3 rounded-lg transition"
-                >
-                    <img
-                        src="https://www.svgrepo.com/show/355037/google.svg"
-                        alt="Google"
-                        className="w-6 h-6"
-                    />
-                    <a
-                        href={`${import.meta.env.VITE_BACKEND_URL}/api/auth/google`}
-                    >
-                        Continue With Google
-                    </a>
-                </button>
-            </div>
-        </div>
-    )
+        <a
+          href={`${import.meta.env.VITE_BACKEND_URL}/api/auth/google`}
+          className="w-60 mx-auto mt-5 flex items-center justify-center gap-3 bg-blue-500 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg transition"
+        >
+          <img
+            src="https://www.svgrepo.com/show/355037/google.svg"
+            alt="Google"
+            className="w-6 h-6"
+          />
+          Continue With Google
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
